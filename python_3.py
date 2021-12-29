3675a230f3d628866c613ad6d3aba62d8bafb407
Tushar Awasthi <84447965+Tush-har@users.noreply.github.com>
2021-12-30 00:50:59 +0530

    Create python_3.py
    set,if elif,else,terniary op.,short circuiting

---
+++ python_3.py
@@ -0,0 +1,61 @@
+#set
+s = {1, 2, 3, 3, 3, 'true', True, 5, 0}
+# print(s)
+# if 99 in s:
+#   print('yes it exist')
+# print("Banana " in s)
+# s.add("Hey");
+# t={"hey i really miss u!","yes u"}
+# t.update(s);
+# print(t);
+# l=[12,22,3];
+# t.update(l);
+# print(t)
+# print(s)
+# s.remove(3)
+# print(s)
+# set1 = {1,2,3}
+# set2 = {1,2}
+
+# set3 = set1.union(set2)
+# print(set3)
+# set4=set1.intersection(set2)
+# set4.clear()
+# print(set4)
+# myset=set1.difference(set2)
+# print(myset)
+# myset=set1.isdisjoint(set2);
+# print(myset)
+# print(set2.issubset(set1))
+# print(set1.issuperset(set2))
+
+# conditional logic
+# username = True
+# password = False
+# if username and password:
+#   print("You can enter in game")
+# else:
+#   print("You can't")
+
+# a = 12
+# b = 13
+# c = 14
+# if a > b:
+#     print("a>b")
+# elif b > c:
+#     print("b>c")
+# else:
+#   print("c is great maong all")
+# a = 330
+# b = 330
+# print("A") if a > b else print("=") if a == b else print("B")
+# age=True;
+# print("yes") if age else print("no")
+
+# short circuiting
+age = True
+# if True or age:
+#   print("Yes")
+# //programm  is efficient
+if False and age:
+    print("Intenially No");
